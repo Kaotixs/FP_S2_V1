6b767635dda0d8d59fcfd3a0b860e4422bf6e777
Kaotixs <cpenky@gmail.com>
2017-11-30 20:41:20 -0800

    updates to some code

--- FP_S2/main.cpp
+++ FP_S2/main.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(720, 1280), "Enemies");
+	sf::RenderWindow window(sf::VideoMode(720, 1280), "Space Andies");
 	
+	Enemies enemy1(sf::Color::Red, sf::Vector2f(30, 20), sf::Vector2f(0, 0));
+	Enemies enemy2(sf::Color::Cyan, sf::Vector2f(30, 20), sf::Vector2f(200, 0));
+	Enemies enemy3(sf::Color::Green, sf::Vector2f(30, 20), sf::Vector2f(500, 0));
 	//sf::CircleShape shape(10.f);
 	//shape.setFillColor(sf::Color::Cyan);
 
@@ -19,6 +22,9 @@ int main()
 
 		window.clear();
 		//window.draw(shape);
+		window.draw(enemy1);
+		window.draw(enemy2);
+		window.draw(enemy3);
 		window.display();
 	}
 
